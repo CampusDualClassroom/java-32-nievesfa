23190c7bf5b201cb0ddfaf018d670f89621919e0
nievesfa <nievesfacorro@gmail.com>
2025-02-10 12:38:20 +0100

    test ok

--- src/main/java/com/campusdual/classroom/Exercise32.java
+++ src/main/java/com/campusdual/classroom/Exercise32.java
@@ -2,17 +2,25 @@ package com.campusdual.classroom;
 
 import com.campusdual.util.Utils;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class Exercise32 {
 
     public static void main(String[] args) {
 
+        String result = generateStringToSave(null);  // Prueba con null para que pida al usuario la entrada
+        System.out.println("Texto a guardar: " + result);
+
+        printToFile(result);       // Guardamos el texto en un archivo
+
     }
 
     public static String generateStringToSave(String string) {
+        if (string != null) {
+            return string;  // Si el texto no es nulo, lo devolvemos tal cual
+        } else {
+            return generateUserInputToSave();  // Si es nulo, pedimos al usuario que lo ingrese
+        }
 
     }
 
@@ -27,8 +35,18 @@ public class Exercise32 {
     }
 
     public static void printToFile(String string) {
+        File file = new File("/Users/nievesfacorro/Documents/CAMPUSDUAL/FULL_STACK/PROJECTS/BACKEND/Classroom/Ejercicio_32_nievesfa/src/main/resources/data.text");  // Especificamos el archivo donde se guardará el texto, se crea antes la ruta para poder guardar en el
+
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(string);  // Escribe la cadena en el archivo
+            System.out.println("El texto ha sido guardado en el archivo '/src/main/resources/data.text'");
+        } catch (Exception e) {
+            System.err.println("Error al guardar el archivo: " + e.getMessage());
+        }
 
     }
 
 
 }
+
+
